714f72eb20173acf411194580432898ff00ab993
Santiago Ferreira <san650@gmail.com>
2016-04-14 09:16:02 -0300

    Use https

--- index.js
+++ index.js
@@ -7,85 +7,85 @@ var albums = {
     id: 1000,
     artist: 'Ramones',
     name: 'Ramones',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-ramones.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-ramones.jpg'
   },
   '1001': {
     id: 1001,
     artist: 'Ramones',
     name: 'Leave Home',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-leave-home.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-leave-home.jpg'
   },
   '1002': {
     id: 1002,
     artist: 'Ramones',
     name: 'Rocket to Russia',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-rocket-to-russia.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-rocket-to-russia.jpg'
   },
   '1003': {
     id: 1003,
     artist: 'Ramones',
     name: 'Road to Ruin',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-road-to-ruin.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-road-to-ruin.jpg'
   },
   '1004': {
     id: 1004,
     artist: 'Ramones',
     name: 'End of the Century',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-end-of-the-century.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-end-of-the-century.jpg'
   },
   '1005': {
     id: 1005,
     artist: 'Ramones',
     name: 'Pleasant Dreams',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-pleasant-dreams.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-pleasant-dreams.jpg'
   },
   '1006': {
     id: 1006,
     artist: 'Ramones',
     name: 'Subterranean Jungle',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-subterranean-jungle.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-subterranean-jungle.jpg'
   },
   '1007': {
     id: 1007,
     artist: 'Ramones',
     name: 'Too Tough to Die',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-too-tough-to-die.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-too-tough-to-die.jpg'
   },
   '1008': {
     id: 1008,
     artist: 'Ramones',
     name: 'Animal Boy',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-animal-boy.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-animal-boy.jpg'
   },
   '1009': {
     id: 1009,
     artist: 'Ramones',
     name: 'Halfway to Sanity',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-halfway-to-sanity.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-halfway-to-sanity.jpg'
   },
   '1010': {
     id: 1010,
     artist: 'Ramones',
     name: 'Brain Drain',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-brain-drain.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-brain-drain.jpg'
   },
   '1011': {
     id: 1011,
     artist: 'Ramones',
     name: 'Mondo Bizarro',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-mondo-bizarro.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-mondo-bizarro.jpg'
   },
   '1012': {
     id: 1012,
     artist: 'Ramones',
     name: 'Acid Eaters',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-acid-eaters.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-acid-eaters.jpg'
   },
   '1013': {
     id: 1013,
     artist: 'Ramones',
     name: 'Adios Amigos',
-    image: 'http://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-adios-amigos.jpg'
+    image: 'https://ng-music.s3-sa-east-1.amazonaws.com/data/ramones-adios-amigos.jpg'
   }
 };
 
